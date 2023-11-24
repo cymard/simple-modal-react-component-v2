807461a75896ae81bc91c752e65d90ab6f60076f
cyrian simard <cyriansimard@gmail.com>
2023-11-24 11:03:27 +0100

    add jsdoc

--- dist/components/Modal.js
+++ dist/components/Modal.js
@@ -8,6 +8,15 @@ var _react = _interopRequireWildcard(require("react"));
 require("./modal.css");
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+/**
+ * Composant Modal utilisé pour afficher un message.
+ * @component
+ * @param {boolean} isOpen - Indique si la modal est ouverte ou fermée.
+ * @param {Function} onClose - Fonction appelé pour fermer la modale.
+ * @param {Function} onExternalClick - Fonction appelée lors d'un clic en dehors de la modal.
+ * @param {React.ReactNode} children - Contenu de la modal.
+ * @returns {JSX.Element} - Élément React représentant la modal.
+ */
 const Modal = _ref => {
   let {
     isOpen,
@@ -17,6 +26,12 @@ const Modal = _ref => {
   } = _ref;
   const paragraphRef = (0, _react.useRef)(null);
   const buttonRef = (0, _react.useRef)(null);
+
+  /**
+   * Gère la fermeture de la modal.
+   * @function
+   * @private
+   */
   const handleClose = () => {
     if (typeof onClose !== 'function') {
       console.error('onClose must be a function');
@@ -24,6 +39,13 @@ const Modal = _ref => {
     }
     onClose();
   };
+
+  /**
+   * Gère les clics en dehors de la modal.
+   * @function
+   * @private
+   * @param {MouseEvent} e - Objet d'événement représentant le clic de la souris.
+   */
   const handleExternalClick = e => {
     if (e.target === paragraphRef.current || e.target === buttonRef.current) {
       return;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,24 @@
 import React, { useRef } from 'react';
 import './modal.css';
 
+/**
+ * Composant Modal utilisé pour afficher un message.
+ * @component
+ * @param {boolean} isOpen - Indique si la modal est ouverte ou fermée.
+ * @param {Function} onClose - Fonction appelé pour fermer la modale.
+ * @param {Function} onExternalClick - Fonction appelée lors d'un clic en dehors de la modal.
+ * @param {React.ReactNode} children - Contenu de la modal.
+ * @returns {JSX.Element} - Élément React représentant la modal.
+ */
 const Modal = ({isOpen, onClose, onExternalClick, children}) => {
     const paragraphRef = useRef(null);
     const buttonRef = useRef(null);
 
+    /**
+     * Gère la fermeture de la modal.
+     * @function
+     * @private
+     */
     const handleClose = () => {
         if (typeof onClose !== 'function') {
             console.error('onClose must be a function');
@@ -14,6 +28,12 @@ const Modal = ({isOpen, onClose, onExternalClick, children}) => {
         onClose();
     }
 
+    /**
+     * Gère les clics en dehors de la modal.
+     * @function
+     * @private
+     * @param {MouseEvent} e - Objet d'événement représentant le clic de la souris.
+     */
     const handleExternalClick = e => {
         if(e.target === paragraphRef.current || e.target === buttonRef.current) {
             return;
